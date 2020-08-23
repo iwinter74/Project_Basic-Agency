0f5f3f7e41ee78c402a7b1976f069cc5a5e329ab
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-08-23 15:47:10 +0200

    v.0.1.1

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let aside = document.getElementById("aside-body")
+
+function myAside() {
+    console.log("working")
+    aside.classList.toggle("show-aside")
+    aside.classList.toggle("hide-aside")
+}
